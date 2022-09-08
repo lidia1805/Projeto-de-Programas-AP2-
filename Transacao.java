2dfd89385d178a783398a622f41118b12378140d
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-09-08 17:05:17 -0300

    Update Transacao.java

--- Transacao.java
+++ Transacao.java
@@ -4,5 +4,11 @@ public class Transacao {
 	private String operacao;
 	private float valor;
 	private String dataHora;
+	
+	public Transacao(String operacao, float valor, String dataHora) {
+		this.operacao = operacao;
+		this.valor = valor;
+		this.dataHora = dataHora;
+	}
 
 }
